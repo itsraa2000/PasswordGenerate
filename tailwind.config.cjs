f4c929da4fda12234f87be3479d0e77a883be963
itsraa2000 <itssraa.2000@gmail.com>
2024-06-14 15:51:31 +0700

    Delete Config Spacing

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,14 +17,6 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    spacing: {
-      '1': '8px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px',
-    },
   },
   plugins: [],
 };
